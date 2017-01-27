5a7f7fd2c061cce2565083032e2661c03601207b
Will Cohen <willcohen@users.noreply.github.com>
2017-01-26 20:32:40 -0500

    Make leiningen project

---
+++ README.md
@@ -0,0 +1,13 @@
+# solutions-4clojure
+
+A Clojure library designed to ... well, that part is up to you.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2017 Will Cohen
+
+Distributed under the MIT License.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject solutions-4clojure "0.1.0-SNAPSHOT"
+  :description "Working through 4clojure problems"
+  :url "http://github.com/willcohen/solutions-4clojure"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
